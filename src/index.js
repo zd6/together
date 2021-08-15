fb54a57d6459d9a1531f928fad09ea6b1e6003be
zd6 <garydai315@hotmail.com>
2021-08-15 21:52:48 +0800

    end of section 1 cleanup boiler code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/style.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Enable hot dynamic update to the page
+
+const rootElement = document.getElementById('root');
+
+function render(){
+  ReactDOM.render(<App />, rootElement)
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function() {
+    setTimeout(render);
+  })
+}
+
+render();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
